65a0d29d37c9ae1451f207f9219df64ba45f3c56
Lucas Ribeiro <lucas.and.rib@gmail.com>
2024-02-04 19:55:17 -0300

    Imports and registers the dataset splitter blueprint.

--- app.py
+++ app.py
@@ -4,16 +4,17 @@ from flask import Flask
 from routes.main_route import main_bp
 from routes.kml_compiler_route import kml_compiler_bp
 from routes.metadata_extractor_route import metadata_extractor_bp
+from routes.dataset_splitter_route import dataset_splitter_bp
 
 # Flask constructor:
 app = Flask(__name__)
 app.secret_key = 'lvc4s'
 
+app.register_blueprint(main_bp)
 app.register_blueprint(kml_compiler_bp)
 app.register_blueprint(metadata_extractor_bp)
-app.register_blueprint(main_bp)
+app.register_blueprint(dataset_splitter_bp)
 
 # Overall execution:
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
- 
